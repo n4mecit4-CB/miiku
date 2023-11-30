93ac0394c847a1e212e57e92feed743773bb1273
n4mecit4 <n4mecit44@gmail.com>
2023-11-30 04:25:31 -0300

    ⲘⲒⲒⲔⴑ

--- plugins/propietario(a)-banchat.js
+++ plugins/propietario(a)-banchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
-m.reply(`*[🛑] ESTE CHAT FUE BANEADO [🛑]*\n\n*❌ESTE CHAT NO TIENE PERMISO PARA USARME ASTA QUE SEA DESBANEADO❌*`)
+m.reply(`*[🛑] ESTE CHAT FUE BANEADO [🛑]*\n\n*❌ESTE CHAT NO TIENE PERMISO PARA USARME HASTA QUE SEA DESBANEADO, ATTE ⲘⲒⲒⲔⴑ❌*`)
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
