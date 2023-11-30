1838486c3617cba4d2bf947d3bff11bd401458aa
n4mecit4-CB <n4mecit44@gmail.com>
2023-11-30 01:41:06 -0300

    ᎷᎥᎥᏦᏬ

--- config.js
+++ config.js
@@ -2,24 +2,24 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['5214531287294', 'CuriosityBot-MD 🍧', true], ['5492266466080', 'Colaborado', true], ['5217294888993'], ['5492266466080'], ['593968585383'], ['51992004117'], ['5492266613038'], ['573106040746'], ['50558124470'], ['5214437863111'], ['51929972576']]
+global.owner = [['56961548695', 'ᎷᎥᎥᏦᏬ💅', true], ['56961548695', 'Colaborador', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
+global.botNumberCode = '+56961548695'
 global.confirmCode = ''
 
 global.animxscans = ['400359205']
-global.suittag = ['994400359205']
+global.suittag = []
 global.mods = []
 global.prems = []
 
-global.packname = 'CuriosityBot-MD'
-global.author = ''
-global.wm = '© CuriosityBot-MD'
-global.wm2 = '𝗖𝗎𝗋𝗂𝗈𝗌𝗂𝗍𝗒 : 𝗕𝗈𝗍'
-global.azami = 'Azami'
-global.cb = 'CuriosityBot-MD'
+global.packname = 'ᎷᎥᎥᏦᏬ💅'
+global.author = 'n4mecit4'
+global.wm = 'ᎷᎥᎥᏦᏬ💅'
+global.wm2 = 'ᎷᎥᎥᏦᏬ💅 : 𝗕𝗈𝗍'
+global.azami = 'n4mecit4'
+global.cb = 'n4mecit4'
 
 global.vs = '1.0.2'
 global.library = 'Baileys'
