c14a75a72954df6c95914303cffebcebc5826284
n4mecit4 <n4mecit44@gmail.com>
2023-11-30 04:51:25 -0300

    ⲘⲒⲒⲔⴑ

--- plugins/juegos-math.js
+++ plugins/juegos-math.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
 conn.math = conn.math ? conn.math : {}
     
 if (args.length < 1) throw `
-*CURIOSITY - MATES*
+*ⲘⲒⲒⲔⴑ - MATES*
 🧮 Dificultades disponibles : 
   
 ${Object.keys(modes).join(' | ')} 
