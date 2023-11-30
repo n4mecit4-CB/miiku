db8c9c740cb09fec976d388b36738944659058ff
n4mecit4 <n4mecit44@gmail.com>
2023-11-30 04:59:44 -0300

    ⲘⲒⲒⲔⴑ

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -33,7 +33,7 @@ let texto = `*🚀 V E L O C I D A D*
 *💻 S E R V I D O R*
 ☯︎ *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
-conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'CURIOSITY - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'ⲘⲒⲒⲔⴑ - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
 
 }
 handler.help = ['ping']
